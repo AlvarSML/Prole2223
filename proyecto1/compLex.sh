a1b21ab529f58061dee4f9a0ab5429cdc70c0e54
alvarsml <alvarsnow@gmail.com>
2022-09-26 21:03:37 +0200

    actualizacion

---
+++ proyecto1/compLex.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -ge 1 ]; then
+    flexfile=$1
+    outputfile="defaultLexCCompilation"
+
+    res = $(flex $flexfile)
+
+    if [ $# -ge 2 ]; then
+        outputfile=$2
+    fi
+
+
+    gcc lex.yy.c -o $outputfile -lfl
+
+    rm lex.yy.c
+    ./$outputfile
+else
+    echo "Num de parametros incorrecto"
+fi
+
+exit
